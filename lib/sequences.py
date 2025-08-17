24e0500bd91a4348c1e998982d8ed87ad03cdf63
Lilian Kirwa <cheronlilian21@gmail.com>
2025-08-17 18:36:57 +0300

    adds all

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+
+    fibonacci = [0]
+    if length > 1:
+        fibonacci.append(1)
+
+    while len(fibonacci) < length:
+        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+
+    print(fibonacci)
